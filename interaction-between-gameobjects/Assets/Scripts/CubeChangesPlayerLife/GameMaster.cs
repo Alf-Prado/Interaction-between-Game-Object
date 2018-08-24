c48a49c9ee59be45bb86bd34216ef01226a73f61
Alf-Prado <alfpradoc@gmail.com>
2018-08-24 18:30:47 -0500

    Ejercicio
    "Cubo cambia de color a esfera" y "Cubo quita vida a esfera" funcional

---
+++ interaction-between-gameobjects/Assets/Scripts/CubeChangesPlayerLife/GameMaster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CubeChangesPlayerLife
+{
+	public class GameMaster : MonoBehaviour {
+        public Sphere Sphere;
+        
+
+        private void OnMouseDown()
+        {
+            Sphere.ChangeLife();
+        }
+
+        // Use this for initialization
+        void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+	}
+}
